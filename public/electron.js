db2c69d0474afcf3385e58e54d2bbc0c4b0f5759
rami0617 <aarmakrr@hanmail.net>
2022-08-27 15:20:49 +0900

    [CHORE]: Initial commit

---
+++ public/electron.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require("electron");
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 900,
+    height: 680,
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: true,
+    },
+  });
+
+  mainWindow.loadURL("http://localhost:3000");
+
+  mainWindow.setResizable(true);
+  mainWindow.on("closed", () => (mainWindow = null));
+  mainWindow.focus();
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
